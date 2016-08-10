28598eebf28c6d94db34233feb66512d904e68f3
Trevor Abell <trevor@keikocode.com>
2016-08-10 12:44:02 -0700

    Add mod lctrl to lower lvl

--- keyboards/preonic/keymaps/trevor/keymap.c
+++ keyboards/preonic/keymaps/trevor/keymap.c
@@ -14,6 +14,7 @@
 #define _DVORAK 2
 #define _LOWER 3
 #define _RAISE 4
+#define _TYPING 8
 #define _ADJUST 16
 
 enum preonic_keycodes {
@@ -39,15 +40,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Rsft(Enter)|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  | GUI  | Alt  | Lower| Ctrl |    Space    | Raise | Alt | RCtl | PgDn | Menu |
+ * | Esc  |Typing| Alt  | Lower| Ctrl |    Space    | Raise | Alt | RCtl | Meh  | Menu |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
   {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL },
-  {MT(MOD_LCTL, KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT},
-  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  MT(MOD_RSFT, KC_ENT) },
-  {KC_LGUI, KC_LGUI, KC_LALT, KC_LCTL, LOWER,   MT(MOD_LALT, KC_SPC),  MT(MOD_RCTL, KC_SPC), RAISE,   KC_LALT, KC_RCTL, KC_MEH,  KC_APP}
+  {MT(MOD_LCTL, KC_ESC), KC_A,KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,KC_L,KC_SCLN, KC_QUOT },
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  MT(MOD_RSFT, KC_ENT) },
+  {KC_LGUI, TG(_TYPING), KC_LALT, KC_LCTL, LOWER,   MT(MOD_LALT, KC_SPC),  MT(MOD_RCTL, KC_SPC), RAISE,   KC_LALT, KC_RCTL, KC_MEH,  KC_APP}
 },
 
 /* Raise
@@ -99,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   ~  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   &  |   *  |   (  |   )  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |  Del |   !  |   @  |   #  |   $  |   %  |   ^  |   _  |   +  |   {  |   }  |  |   |
+ * |  MT(LCtrl, Del) |   !  |   @  |   #  |   $  |   %  |   ^  |   _  |   +  |   {  |   }  |  |   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |   !  |   @  |   #  |   $  |   %  |   ^  |ISO ~ |ISO | |  Home|  Up  | End  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -110,11 +111,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                                     H1                         H2
   {KC_TILD, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC},
   {KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL},
-  {KC_DEL , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
+  {MT(MOD_LCTL, KC_DEL)  , KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE},
   {_______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, S(KC_NUHS),S(KC_NUBS), KC_HOME, KC_UP, KC_END},
   {_______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_LEFT, KC_DOWN, KC_RGHT}
-}
+},
 
+
+/* Typing
+ * ,-----------------------------------------------------------------------------------.
+ * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Del  |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Rsft(Enter)|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | GUI  |      | Alt  | Lower| Ctrl |    Space    | Raise | Alt | RCtl | Meh  | Menu |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_TYPING] = {
+  {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
+  {KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL },
+  {MT(MOD_LCTL, KC_ESC), KC_A,KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,KC_L,KC_SCLN, KC_QUOT },
+  {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  MT(MOD_RSFT, KC_ENT) },
+  {KC_LGUI, _______, KC_LALT, KC_LCTL, _______,   KC_SPC,  KC_SPC,  _______,   KC_LALT, KC_RCTL, KC_MEH,  KC_APP}
+}
 };
 
 #ifdef AUDIO_ENABLE
