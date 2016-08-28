8910619b7c779719c4a7c8738e01fde885c602f3
Trevor Abell <trevor.abell@parsus.com>
2016-08-28 00:54:00 -0700

    Make ' ralt(')

--- keyboards/preonic/keymaps/trevor/keymap.c
+++ keyboards/preonic/keymaps/trevor/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Rsft(Del)  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  "   |
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | Ralt(")   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Rsft(Enter)|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = {
   {KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC},
   {MT(MOD_LSFT, KC_TAB),  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, MT(MOD_RSFT, KC_DEL) },
-  {MT(MOD_LCTL, KC_ESC), KC_A,KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,KC_L,KC_SCLN, KC_QUOT },
+  {MT(MOD_LCTL, KC_ESC), KC_A,KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,KC_L,KC_SCLN, MT(MOD_RALT, KC_QUOT) },
   {KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  MT(MOD_RSFT, KC_ENT) },
   {KC_LGUI, TG(_TYPING), KC_LALT, KC_LCTL, LOWER, MT(MOD_LALT, KC_SPC), MT(MOD_RCTL, KC_TAB), LT(_RAISE, KC_ENT),   KC_LALT, KC_RCTL, KC_MEH,  KC_APP}
 },
