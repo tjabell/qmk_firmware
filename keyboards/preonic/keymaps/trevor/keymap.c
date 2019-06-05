cc41dbc4cf28f4ce7fdd7683e9764ba838c04b90
Trevor Abell <trevor.abell@parsus.com>
2019-06-05 15:45:48 -0700

    refactor(keymap):Copy from default for now to use the standard

--- keyboards/preonic/keymaps/trevor/keymap.c
+++ keyboards/preonic/keymaps/trevor/keymap.c
@@ -1,28 +1,25 @@
-#include "preonic.h"
-#include "action_layer.h"
-#include "eeconfig.h"
-#ifdef AUDIO_ENABLE
-  #include "audio.h"
-#endif
+#include QMK_KEYBOARD_H
+#include "muse.h"
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-#define _QWERTY 0
-#define _COLEMAK 1
-#define _DVORAK 2
-#define _LOWER 3
-#define _RAISE 4
-#define _TYPING 8
-#define _ADJUST 16
+enum preonic_layers {
+  _QWERTY,
+  _COLEMAK,
+  _DVORAK,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+  _TYPING
+};
 
 enum preonic_keycodes {
   QWERTY = SAFE_RANGE,
+  COLEMAK,
+  DVORAK,
   LOWER,
   RAISE,
   BACKLIT
 };
+
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
